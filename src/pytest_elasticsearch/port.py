69d2eabb0b4bd93f54fff0a44896222228072596
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2018-04-05 15:13:08 +0200

    pylama fixes

--- src/pytest_elasticsearch/port.py
+++ src/pytest_elasticsearch/port.py
@@ -15,6 +15,7 @@
 
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytest-elasticsearch.  If not, see <http://www.gnu.org/licenses/>.
+"""Helpers for port-for package."""
 from itertools import chain
 
 import port_for
