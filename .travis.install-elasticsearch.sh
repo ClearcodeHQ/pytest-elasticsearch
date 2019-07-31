2f8c1215a4bbec80d90bd646a216360ba4e9606f
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2019-07-31 17:26:00 +0200

    Refresh elasticsearch versions used for tests

--- .travis.install-elasticsearch.sh
+++ .travis.install-elasticsearch.sh
@@ -3,9 +3,16 @@ function install_from_zip {
     wget $1 -O out
     unzip out
     sudo mv $2 /opt/$2
+    /opt/$2/bin/elasticsearch -Vv
 }
 
-install_from_zip https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.zip elasticsearch-1.5.2
-install_from_zip https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-2.4.6.zip elasticsearch-2.4.6
-install_from_zip https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.7.zip elasticsearch-5.6.7
-install_from_zip https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.3.zip elasticsearch-6.2.3
+function install_from_targz {
+    wget $1 -O out
+    tar -zxf out
+    sudo mv $2 /opt/$2
+    /opt/$2/bin/elasticsearch -Vv
+}
+
+install_from_zip https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.16.zip elasticsearch-5.6.16
+install_from_zip https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.2.zip elasticsearch-6.8.2
+install_from_targz https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.3.0-linux-x86_64.tar.gz elasticsearch-7.3.0
