e0211d33c869b33278410f35d8109cedab22e964
Grzegorz Śliwiński <g.sliwinski@clearcode.cc>
2019-10-25 10:56:28 +0200

    Attempt to autoheal elassticsearch isntallation

--- .travis.install-elasticsearch.sh
+++ .travis.install-elasticsearch.sh
@@ -7,6 +7,8 @@ function install_from_zip {
     sudo mv $2 $INSTALL_PATH
   fi
   /opt/es/$2/bin/elasticsearch -Vv
+  remove_if_erroneus $2
+
 }
 
 function install_from_targz {
@@ -17,6 +19,16 @@ function install_from_targz {
     sudo mv $2 $INSTALL_PATH
   fi
   /opt/es/$2/bin/elasticsearch -Vv
+  remove_if_erroneus $2
+}
+
+function remove_if_erroneus {
+  if [ ! -f "/opt/es/$1/bin/elasticsearch" ]; then
+    ls -l /opt/es/
+    ls -l /opt/es/$1
+    ls -l /opt/es/$1/bin
+    rm -r /opt/es/$1
+  fi
 }
 
 install_from_zip https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.16.zip elasticsearch-5.6.16
